5143ecf6840d039733ad4a4c1b2546fccfed5ec6
deepak <deepaksharma91735@gmail.com>
2023-01-25 18:19:38 +0530

    session topic complete

---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+
+# Session in Python Django
+
+Here you learn about the session and every line of code explaining in commend. I hope you can understand.
+You can also see the session time ,expire on the webpage.
+
+
+
+
+## Session Topic    
+
+- Set Session
+- Get Session
+- Delete Session
+
+
+## Authors
+
+- [Deepak Sharma](https://www.github.com/dipu-developer)
+
+
+## Support
+
+For support, email deepaksharma91735@gamil.com.com or join our Slack channel.
+
--- app/views.py
+++ app/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 #create the session with the cokies
 #session is same like cookies but data are storedin the formate of name and key
 def setsession(request):
-    request.session['name'] = 'deepak'          #this create session key is name and value is deepak
-    request.session['surname'] = "sharma"
-    request.session.set_expiry(2000)            #set the session in second and 0 second mean browserclose session close
+    request.session['name'] = 'bhuam'          #this create session key is name and value is deepak
+    request.session['surname'] = "bham"
+    request.session.set_expiry(100)            #set the session in second and 0 second mean browserclose session close
+    # request.session.set_expiry(0)            #the value of get_expire_at_brower_close() become true and it expire the
+                                                #when the user close the window
     return render(request,'setsession.html')    #we dont have to send any other think it will first argument is session 
 
 #featching the session
 def getsession(request):
-    # name = request.session['name']
+    # name = request.session['name']            #the value id none it will through the error
     name = request.session.get('name',default='Guest')  #if the user not present it will return the Guest
     surname = request.session.get('surname',default='Guest surname')  #if the user not present it will return the Guest
 
@@ -23,6 +25,8 @@ def getsession(request):
     return render(request,'getsession.html',{'name':name,'surname':surname})
 
 def delsession(request):
-    if 'name'in request.session:
-        del request.session['name']
+    # if 'name'in request.session:
+    #     del request.session['name']
+    request.session.flush()                 #we have to clear the session from database otherwise it save thesesssion in the databae  
+    request.session.clear_expired()         
     return render(request,'delsession.html')
